27f0b726ff38fbe6b2610a74980541c8cfb651a0
pbz <pbz@github.com>
2018-03-14 12:58:51 -0400

    keyboards/planck/keymaps/pbz/keymap.c: Update numpad layer to be more "standard"

--- keyboards/planck/keymaps/pbz/keymap.c
+++ keyboards/planck/keymaps/pbz/keymap.c
@@ -190,20 +190,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Num
  * ,-----------------------------------------------------------------------------------.
- * |      |  9   |  8   |  7   |  *   |      |      |  7   |  8   |  9   |  *   |      |
+ * |      |  -   |  9   |  8   |  7   |      |      |  7   |  8   |  9   |  -   |      |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |      |  6   |  5   |  4   |  /   |      |      |  4   |  5   |  6   |  /   |      |
+ * |      |  +   |  6   |  5   |  4   |      |      |  4   |  5   |  6   |  +   |      |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      |  3   |  2   |  1   |  +   |      |      |  1   |  2   |  3   |  +   |      |
+ * |      |  /   |  3   |  2   |  1   |  =   |  =   |  1   |  2   |  3   |  /   |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |  =   |  .   |  0   |  -   | Enter|      |  0   |  .   |  =   |  -   | Enter|
+ * |      |  *   |  .   |  ,   |  0   |Enter |Enter |  0   |  ,   |  .   |  *   |      |
  * `-----------------------------------------------------------------------------------'
  */
 [_NUM] = {
-  {_______,     KC_KP_9,   KC_KP_8, KC_KP_7, KC_KP_ASTERISK,     _______, _______, KC_KP_7, KC_KP_8,       KC_KP_9, KC_KP_ASTERISK,     _______},
-  {_______,     KC_KP_6,   KC_KP_5, KC_KP_4,    KC_KP_SLASH,     _______, _______, KC_KP_4, KC_KP_5,       KC_KP_6,    KC_KP_SLASH,     _______},
-  {_______,     KC_KP_3,   KC_KP_2, KC_KP_1,     KC_KP_PLUS,     _______, _______, KC_KP_1, KC_KP_2,       KC_KP_3,     KC_KP_PLUS,     _______},
-  {_______, KC_KP_EQUAL, KC_KP_DOT, KC_KP_0,       KC_MINUS, KC_KP_ENTER, _______, KC_KP_0, KC_KP_DOT, KC_KP_EQUAL,       KC_MINUS, KC_KP_ENTER}
+  {_______,       KC_MINUS,     KC_KP_9,     KC_KP_8, KC_KP_7,     _______,     _______, KC_KP_7,     KC_KP_8,       KC_KP_9,       KC_MINUS,     _______},
+  {_______,     KC_KP_PLUS,     KC_KP_6,     KC_KP_5, KC_KP_4,     _______,     _______, KC_KP_4,     KC_KP_5,       KC_KP_6,     KC_KP_PLUS,     _______},
+  {_______,    KC_KP_SLASH,     KC_KP_3,     KC_KP_2, KC_KP_1, KC_KP_EQUAL, KC_KP_EQUAL, KC_KP_1,     KC_KP_2,       KC_KP_3,    KC_KP_SLASH,     _______},
+  {_______, KC_KP_ASTERISK,   KC_KP_DOT, KC_KP_COMMA, KC_KP_0, KC_KP_ENTER, KC_KP_ENTER, KC_KP_0, KC_KP_COMMA,     KC_KP_DOT, KC_KP_ASTERISK,     _______}
 },
 
 // FN
